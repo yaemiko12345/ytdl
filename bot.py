57ff3106a6f969646ed1322f0199a9daa1842adc
yaemiko12345 <vaviloid98+1@gmail.com>
2024-10-01 06:07:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,9 +10,9 @@ from Youtube.config import Config
 # Create a Pyrogram client
 app = Client(
     "my_bot",
-    api_id=Config.API_ID,25604709
-    api_hash=Config.API_HASH,6a53ef81ff567058c2aa8ef1f9fa0ba5
-    bot_token=Config.BOT_TOKEN,7656527709:AAFaxqRDDlE9g49rRD-cLib3tte0CcnCrJs
+    api_id=Config.25604709
+    api_hash=Config.6a53ef81ff567058c2aa8ef1f9fa0ba5
+    bot_token=Config.7656527709:AAFaxqRDDlE9g49rRD-cLib3tte0CcnCrJs
     plugins=dict(root="Youtube")
 )
 
